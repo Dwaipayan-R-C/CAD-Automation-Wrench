07b427f50441d30966152ef1edcf5c70b6c5f952
Dwaipayan Roy Chowdhury <75967964+Dwaipayan-R-C@users.noreply.github.com>
2020-12-14 13:58:00 +0530

    Add files via upload

---
+++ StanLey_Final/Stanley1/Form1.Designer.vb
@@ -0,0 +1,177 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.ofd = New System.Windows.Forms.OpenFileDialog()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.ComboBox2 = New System.Windows.Forms.ComboBox()
+        Me.TrackBar1 = New System.Windows.Forms.TrackBar()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        CType(Me.TrackBar1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.ItemHeight = 18
+        Me.ComboBox1.Location = New System.Drawing.Point(185, 69)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(100, 26)
+        Me.ComboBox1.TabIndex = 0
+        '
+        'ofd
+        '
+        Me.ofd.FileName = "OpenFileDialog1"
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(117, 12)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(119, 32)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Browse File"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.Location = New System.Drawing.Point(117, 227)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(119, 26)
+        Me.Button2.TabIndex = 3
+        Me.Button2.Text = "Generate"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'ComboBox2
+        '
+        Me.ComboBox2.DropDownWidth = 100
+        Me.ComboBox2.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ComboBox2.FormattingEnabled = True
+        Me.ComboBox2.ItemHeight = 18
+        Me.ComboBox2.Location = New System.Drawing.Point(185, 119)
+        Me.ComboBox2.Name = "ComboBox2"
+        Me.ComboBox2.Size = New System.Drawing.Size(100, 26)
+        Me.ComboBox2.TabIndex = 5
+        '
+        'TrackBar1
+        '
+        Me.TrackBar1.Location = New System.Drawing.Point(189, 176)
+        Me.TrackBar1.Maximum = 2
+        Me.TrackBar1.Name = "TrackBar1"
+        Me.TrackBar1.Size = New System.Drawing.Size(104, 45)
+        Me.TrackBar1.TabIndex = 6
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(57, 72)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(107, 18)
+        Me.Label1.TabIndex = 7
+        Me.Label1.Text = "Handle Color"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(96, 122)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(68, 18)
+        Me.Label2.TabIndex = 8
+        Me.Label2.Text = "Material"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(63, 176)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(101, 18)
+        Me.Label3.TabIndex = 9
+        Me.Label3.Text = "Jaw Positon"
+        '
+        'Button3
+        '
+        Me.Button3.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button3.Location = New System.Drawing.Point(117, 347)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(119, 29)
+        Me.Button3.TabIndex = 10
+        Me.Button3.Text = "Show File"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button4.Location = New System.Drawing.Point(117, 285)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(119, 29)
+        Me.Button4.TabIndex = 11
+        Me.Button4.Text = "Refresh"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(404, 388)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TrackBar1)
+        Me.Controls.Add(Me.ComboBox2)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.TrackBar1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents ofd As OpenFileDialog
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents ComboBox2 As ComboBox
+    Friend WithEvents TrackBar1 As TrackBar
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+End Class
---
+++ StanLey_Final/Stanley1/Form1.vb
@@ -0,0 +1,196 @@
+﻿Public Class Form1
+
+    Dim swApp As SldWorks.SldWorks
+    Dim swModel As SldWorks.ModelDoc2
+    Dim swModel1 As SldWorks.ModelDoc2
+    Dim swCompModel As SldWorks.ModelDoc2
+    Dim swSelMgr As SldWorks.SelectionMgr
+    Dim swComp As SldWorks.Component2
+    Dim vMatProp As Object
+    Dim Part As Object
+    Dim bRet As Boolean
+    Dim booleanpi As Boolean
+    Dim errors As Long
+    Dim warnings As Long
+    Dim var As String
+    Dim red As Integer
+    Dim grn As Integer
+    Dim bl As Integer
+    Dim Directory As String
+    Dim Matvalue1 As String
+    'Dim Matvalue2 As String
+    'Dim Visible As Boolean
+    Dim swModelDocExt As SldWorks.ModelDocExtension
+    Dim swAppearance As SldWorks.RenderMaterial
+    Dim bool2 As Boolean
+    Dim strName As String
+    Dim nDecalID As Long
+    Dim bool As Boolean
+    Dim boolea As Boolean
+    Dim err As Long
+    Dim warn As Long
+    Dim Boolst As Boolean
+    Dim longstatus As Long, longwarnings As Long
+    Dim Longi As Long
+    Dim Directory1 As String
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.ComboBox1.Items.Add("Red")
+        Me.ComboBox1.Items.Add("Green")
+        Me.ComboBox1.Items.Add("Blue")
+
+        Me.ComboBox2.Items.Add("Brushed Aluminium")
+        Me.ComboBox2.Items.Add("Brushed Brass")
+    End Sub
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+
+        If (ofd.ShowDialog() = DialogResult.OK) Then
+            Directory = ofd.FileName
+            Dim FolderPath As String = Directory
+            Directory1 = (FolderPath.Substring(0, FolderPath.LastIndexOf("\")))
+            'Debug.Print(Directory1 + "\text")
+        End If
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        swApp = New SldWorks.SldWorks()
+        swModel1 = swApp.OpenDoc6(Directory1 + "\GeneratedModel.SLDASM", SwConst.swDocumentTypes_e.swDocASSEMBLY, SwConst.swOpenDocOptions_e.swOpenDocOptions_Silent, "", err, warn)
+        swModel1.Visible = True
+
+        'Dim swErrors As Long
+        'Dim swWarnings As Long
+        'boolea = swModel.Save3(1, swErrors, swWarnings)
+        'swModel = swApp.OpenDoc6("E:\Tandemloop\Project\WIP\Model_Specifications\GRIP-TEXTURE.sldprt", SwConst.swDocumentTypes_e.swDocPART, SwConst.swOpenDocOptions_e.swOpenDocOptions_Silent, "", err, warn)
+        'swModelDocExt = swModel.Extension
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Dim myProcess() As Process = System.Diagnostics.Process.GetProcessesByName("SLDWORKS")
+
+        For Each myKill As Process In myProcess
+            myKill.Kill()
+
+        Next
+    End Sub
+
+    Public Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        swApp = New SldWorks.SldWorks()
+
+
+        'swModel = swApp.OpenDoc6("C:\Stanley\Final\GRIP-TEXTURE.sldprt", SwConst.swDocumentTypes_e.swDocPART, SwConst.swOpenDocOptions_e.swOpenDocOptions_Silent, "", err, warn)
+        swModel = swApp.OpenDoc6(Directory1 + "\GRIP-TEXTURE.sldprt", SwConst.swDocumentTypes_e.swDocPART, SwConst.swOpenDocOptions_e.swOpenDocOptions_Silent, "", err, warn)
+        swModelDocExt = swModel.Extension
+
+        swModel.ClearSelection2(True)
+
+        If Me.ComboBox2.SelectedIndex = 0 Then
+            Matvalue1 = "C:\Program Files\SOLIDWORKS Corp\SOLIDWORKS\IRay\mdl\solidworks\materials\metal\aluminum\brushed_aluminum.mdl"
+            strName = Matvalue1
+            swAppearance = swModelDocExt.CreateRenderMaterial(strName)
+            bool2 = swAppearance.AddEntity(swModel)
+            bool2 = swModelDocExt.AddRenderMaterial(swAppearance, nDecalID)
+            Dim swErrors As Long
+            Dim swWarnings As Long
+
+            'bool = swModel.EditRebuild3()
+            swModel.Save3(1, swErrors, swWarnings)
+
+        ElseIf Me.ComboBox2.SelectedIndex = 0 Then
+            Matvalue1 = "C:\Program Files\SOLIDWORKS Corp\SOLIDWORKS\IRay\mdl\solidworks\materials\metal\brass\brushed_brass.mdl"
+            strName = Matvalue1
+            swAppearance = swModelDocExt.CreateRenderMaterial(strName)
+            bool2 = swAppearance.AddEntity(swModel)
+            bool2 = swModelDocExt.AddRenderMaterial(swAppearance, nDecalID)
+            Dim swErrors As Long
+            Dim swWarnings As Long
+
+            bool = swModel.Save3(1, swErrors, swWarnings)
+            'bool = swModel.EditRebuild3()
+        End If
+        Boolst = swModel.EditRebuild3()
+
+        swModel.ClearSelection2(True)
+        ' Apply the specified appearance to add to the model
+
+
+        'Debug.Print(bool)
+
+        swModel = swApp.OpenDoc6(Directory, SwConst.swDocumentTypes_e.swDocASSEMBLY, SwConst.swOpenDocOptions_e.swOpenDocOptions_Silent, "", errors, warnings)
+
+        If (swModel Is Nothing) Then
+            MessageBox.Show("End Previously Running Solidworks File")
+
+        Else
+
+            swSelMgr = swModel.SelectionManager
+
+            booleanpi = swModel.Extension.SelectByID2("GRIP-TEXTURE-1@Spanner", "COMPONENT", 0, 0, 0, False, 0, Nothing, 0)
+
+            swComp = swSelMgr.GetSelectedObjectsComponent2(1)
+            vMatProp = swComp.MaterialPropertyValues
+
+            'Debug.Print("sdvdsv")
+
+            If (vMatProp Is Nothing) Then
+                ' Empty if no component-level colors specified,
+                ' so get from underlying model
+                swCompModel = swComp.GetModelDoc
+                If swCompModel Is Nothing Then
+                    ' Component is lightweight
+                    Debug.Print("Selected component is lightweight; exiting macro.")
+                    Exit Sub
+                End If
+
+                vMatProp = swCompModel.MaterialPropertyValues
+            End If
+
+            If Me.ComboBox1.SelectedIndex = 0 Then
+                vMatProp(0) = 1
+                vMatProp(1) = 0
+                vMatProp(2) = 0
+
+            ElseIf Me.ComboBox1.SelectedIndex = 1 Then
+                vMatProp(0) = 0
+                vMatProp(1) = 1
+                vMatProp(2) = 0
+
+            Else
+                vMatProp(0) = 0
+                vMatProp(1) = 0
+                vMatProp(2) = 1
+            End If
+            swComp.MaterialPropertyValues = vMatProp
+
+            ' Deselect component to see new color
+            swModel.ClearSelection2(True)
+        End If
+
+
+        'Part = swApp.OpenDoc6("C:\Stanley\Final\Spanner.SLDASM", SwConst.swDocumentTypes_e.swDocASSEMBLY, SwConst.swOpenDocOptions_e.swOpenDocOptions_Silent, "", errors, warnings)
+        Part = swApp.OpenDoc6(Directory, SwConst.swDocumentTypes_e.swDocASSEMBLY, SwConst.swOpenDocOptions_e.swOpenDocOptions_Silent, "", errors, warnings)
+        Dim myDimension As Object
+        If TrackBar1.Value.ToString() = 0 Then
+            myDimension = Part.Parameter("D1@LimitDistance1")
+            myDimension.SystemValue = 0.001
+
+        ElseIf TrackBar1.Value.ToString() = 2 Then
+            myDimension = Part.Parameter("D1@LimitDistance1")
+            myDimension.SystemValue = 0.03479
+
+        Else
+            myDimension = Part.Parameter("D1@LimitDistance1")
+            myDimension.SystemValue = 0.014
+
+        End If
+
+
+        Boolst = Part.EditRebuild3()
+        Part.ClearSelection2(True)
+        'Longi = swModel.SaveAs3("C:\Stanley\Final\Output\Stanley_Output\GeneratedModel.SLDASM", 0, 2)
+        Longi = swModel.SaveAs3(Directory1 + "\GeneratedModel.SLDASM", 0, 2)
+    End Sub
+
+End Class
